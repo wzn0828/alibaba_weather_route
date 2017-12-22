56435dfb6917a40e685abca496b6166ce72af27e
wzn0828 <18001279185@163.com>
2017-12-22 16:50:17 +0800

    add current as return when  use a_star_search_3D

--- tools/A_star_alibaba.py
+++ tools/A_star_alibaba.py
@@ -223,10 +223,10 @@ def A_star_search_3D(cf):
             # the goal location spans from all the time stamps--> as long as we reach the goal in any time stamp,
             # we say we have reached the goal
             goal_loc_3D = [(goal_loc[0], goal_loc[1], t) for t in range(cf.time_length)]
-            came_from, cost_so_far = a_star_search_3D(diagram, start_loc_3D, goal_loc_3D)
+            came_from, cost_so_far, current = a_star_search_3D(diagram, start_loc_3D, goal_loc_3D)
 
             route_list = []
-            current_loc = list(set(goal_loc_3D) & set(came_from.keys()))
+            current_loc = list(set(goal_loc_3D) & set(current)) # instead of came_from.keys() by current
 
             if not len(current_loc):
                 print('We cannot reach the goal city: %d, continue!' % goal_city)
@@ -348,10 +348,10 @@ def A_star_3D_worker(cf, day, goal_city):
     # the goal location spans from all the time stamps--> as long as we reach the goal in any time stamp,
     # we say we have reached the goal
     goal_loc_3D = [(goal_loc[0], goal_loc[1], t) for t in range(cf.time_length)]
-    came_from, cost_so_far = a_star_search_3D(diagram, start_loc_3D, goal_loc_3D)
+    came_from, cost_so_far, current = a_star_search_3D(diagram, start_loc_3D, goal_loc_3D)
 
     route_list = []
-    current_loc = list(set(goal_loc_3D) & set(came_from.keys()))
+    current_loc = list(set(goal_loc_3D) & set(current))
 
     if not len(current_loc):
         print('We cannot reach the goal city: %d, continue!' % goal_city)
